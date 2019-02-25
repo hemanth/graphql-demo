5ab29bd69b2dbb697982172320cf92cb06e42f76
Hemanth.HM <hhegadehallimadh@paypal.com>
2019-02-25 18:50:34 +0530

    Migrated books filter from v1.

--- schema/typedefs.js
+++ schema/typedefs.js
@@ -10,12 +10,20 @@ const typeDefs = gql`
     url: String!
   }
 
-  type Query {
-    allBooks: [Book!]!
-  }
-
   type Mutation {
     createBook(title: String!, author: String!): Book
+  }
+
+  type Query {
+    allBooks(filter: bookFilter): [Book!]!
+  }
+  
+  input bookFilter {
+    id: ID
+    title: String
+    author: String
+    isbn: String
+    url: String
   }
 `;
 
